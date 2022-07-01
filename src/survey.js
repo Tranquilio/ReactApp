c81b397ddbbfbfe6c7d9c9fc10e783b4684b82b9
Sridharan Arvind Srinivasan <arv1920@gmail.com>
2022-07-02 03:57:43 +0800

    Integrate backend into survey

--- src/survey.js
+++ src/survey.js
@@ -4,9 +4,10 @@ import { registerCoreBlocks } from "@quillforms/react-renderer-utils";
 import "./styles.css";
 import "./custom-block";
 import "./custom-block2";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 registerCoreBlocks();
+var blocks = [];
 
 const Survey = () => {
 
@@ -16,96 +17,127 @@ const Survey = () => {
         });
         var data =  await result.json();
         console.log(data);
+        getBlocks(data["list"])
+        setLoad(true);
     }
 
+    const [loaded,setLoad] = useState(false);
+
     useEffect(() => {
-        
-        
         getQuestions();
     }, [])
 
-    return (
+    function generateId() {
+        return Math.random().toString(36).slice(2);
+    }
 
-    <div style={{ width: "100%", height: "100vh" }}>
-        <Form
-        formId="1"
-        formObj={{
-            blocks: [
-            {
-                name: "welcome-screen",
-                id: "jg1401r",
-                attributes: {
+    var standard = [
+        {
+            name: "welcome-screen",
+            id: "jg1401r",
+            attributes: {
                 label: "Welcome to our survey, ready when you are!",
                 attachment: {
                     type: "image",
                     url: "",
-                    
                 }
-                }
+            }
+        },
+        {
+            name: "my-custom-block-unique-name",
+            id: "jg1402r",
+            attributes: {
+            label: "Question",
+            required: true,
+            start: 1,
+            end: 6
             },
-            {
+        },
+        {
+            name: "statement",
+            id: "g91imf1023",
+            attributes: {
+            label: "You are doing great so far!",
+            buttonText: "Continue",
+            quotationMarks: true
+            }
+        },  
+        {
+            name: "statement",
+            id: "jg1403r",
+            color: "#FFFFFF",
+            attributes: {
+            label: "Tranquilio's vision is to empower the 21st Century Workforce.",
+            description: "By understanding what's causing us stress and burnout at work, we can start the conversation on how organizations approach workplace mental wellbeing. We appreciate you for completing this assessment!",
+            quotationMarks: true,
+            buttonText: "Fantastic!",
+            }, 
+        },
+    ]
+
+    function getBlocks(questions) {
+        blocks = [];
+        for (var element of standard) {
+            blocks.push(element);
+        }
+        for(var element of questions) {
+            var item = {
                 name: "my-custom-block-unique-name",
-                id: "jg1402r",
+                id: generateId(),
                 attributes: {
-                label: "Question",
+                label: element.question,
                 required: true,
                 start: 1,
                 end: 6
                 },
-            },
-            {
-                name: "statement",
-                id: "g91imf1023",
-                attributes: {
-                label: "You are doing great so far!",
-                buttonText: "Continue",
-                quotationMarks: true
-                }
-            },  
-            {
-                name: "statement",
-                id: "jg1403r",
-                color: "#FFFFFF",
-                attributes: {
-                label: "Tranquilio's vision is to empower the 21st Century Workforce.",
-                description: "By understanding what's causing us stress and burnout at work, we can start the conversation on how organizations approach workplace mental wellbeing. We appreciate you for completing this assessment!",
-                quotationMarks: true,
-                buttonText: "Fantastic!",
-                }, 
-            },             
-            ],
-            settings: {
-            animationDirection: "vertical",
-            disableWheelSwiping: false,
-            disableNavigationArrows: false,
-            disableProgressBar: false
-            },
-            theme: {
-            font: "Roboto",
-            buttonsBgColor: "#9b51e0",
-            logo: {
-                src: ""
-            },
-            backgroundImage: "https://images.pexels.com/photos/6331085/pexels-photo-6331085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            backgroundColor: "",
-            questionsColor: "#fff",
-            answersColor: "#0aa7c2",
-            buttonsFontColor: "#000",
-            buttonsBgColor: "#fff",
-            buttonsBorderRadius: 10,
-            errorsFontColor: "#fff",
-            errorsBgColor: "#f00",
-            progressBarFillColor: "#9b51e0",
-            progressBarBgColor: "#ccc"
             }
-        }}
-        onSubmit={(data, { completeForm, setIsSubmitting }) => {
-            setTimeout(() => {
-            setIsSubmitting(false);
-            completeForm();
-            }, 500);
-        }}
-        />
+            blocks.splice(2, 0, item);
+        }
+        console.log(standard);
+        console.log(blocks);
+    }
+
+    return (
+
+    <div style={{ width: "100%", height: "100vh" }}>
+        { (!loaded) ? null : 
+            <Form
+            formId="1"
+            formObj={{
+                blocks: blocks,
+                settings: {
+                animationDirection: "vertical",
+                disableWheelSwiping: false,
+                disableNavigationArrows: false,
+                disableProgressBar: false
+                },
+                theme: {
+                font: "Roboto",
+                buttonsBgColor: "#9b51e0",
+                logo: {
+                    src: ""
+                },
+                backgroundImage: "https://images.pexels.com/photos/6331085/pexels-photo-6331085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                backgroundColor: "",
+                questionsColor: "#fff",
+                answersColor: "#0aa7c2",
+                buttonsFontColor: "#000",
+                buttonsBgColor: "#fff",
+                buttonsBorderRadius: 10,
+                errorsFontColor: "#fff",
+                errorsBgColor: "#f00",
+                progressBarFillColor: "#9b51e0",
+                progressBarBgColor: "#ccc"
+                }
+            }}
+            onSubmit={(data, { completeForm, setIsSubmitting }) => {
+                setTimeout(() => {
+                setIsSubmitting(false);
+                completeForm();
+                }, 500);
+            }}
+            />
+        }
     </div>
 
   );
