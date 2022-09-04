6e2c490ae45c74e5844fa402a57fc4da82a7259f
VishalJeyaram <vishalsonic2000@gmail.com>
2022-09-04 16:28:41 +0800

    Survey preview and email service completed

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const fetch = (...args) =>
-	import('node-fetch').then(({default: fetch}) => fetch(...args));
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
 // apiRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests 
 const employeeRoutes = express.Router();
- 
+
 // This will help us connect to the database
 const dbo = require("../conn");
 
@@ -13,7 +13,7 @@ const dbo = require("../conn");
 function getAvg(object) {
     //Ignore first two elements of employeeInfo and calc average of scores per individual
     avg = 0
-    for(i=2; i<object.employeeInfo.length; i++) {
+    for (i = 2; i < object.employeeInfo.length; i++) {
         avg = avg + object.employeeInfo[i]
     }
     avg = avg / (object.employeeInfo.length - 1)
@@ -55,6 +55,7 @@ function getAvg(object) {
 // This section will help you get a list of all the employee data. (To have average score -> use update to update the database)
 employeeRoutes.route("/api/employees/all").get(function (req, res) {
     let db_connect = dbo.getDb();
+    console.log(db_connect)
     cursor = db_connect.collection("typeform-response").find({});
     cursor.toArray(function (err, result) {
         if (err) throw err;
@@ -64,7 +65,7 @@ employeeRoutes.route("/api/employees/all").get(function (req, res) {
 
 employeeRoutes.route("/api/employees/:id").get(function (req, res) {
     let db_connect = dbo.getDb();
-    cursor = db_connect.collection("typeform-response").find({companyName: req.params.id});
+    cursor = db_connect.collection("typeform-response").find({ companyName: req.params.id });
     cursor.toArray(function (err, result) {
         if (err) throw err;
         res.json(result);
@@ -74,12 +75,13 @@ employeeRoutes.route("/api/employees/:id").get(function (req, res) {
 // Used for overall wellbeing score
 employeeRoutes.route("/api/employees/scores/all").get(function (req, res) {
     let db_connect = dbo.getDb();
+    console.log(db_connect)
     cursor = db_connect.collection("typeform-response").aggregate([
-        { $group: {_id:null, average: { $avg: "$averageScore" } } },
+        { $group: { _id: null, average: { $avg: "$averageScore" } } },
     ]);
     cursor.toArray(function (err, result) {
         if (err) throw err;
-        res.json({"Average Score" : result[0].average * 100/6});     
+        res.json({ "Average Score": result[0].average * 100 / 6 });
     });
 });
 
@@ -87,11 +89,11 @@ employeeRoutes.route("/api/employees/scores/:id").get(function (req, res) {
     let db_connect = dbo.getDb();
     cursor = db_connect.collection("typeform-response").aggregate([
         { $match: { companyName: req.params.id } },
-        { $group: {_id:null, average: { $avg: "$averageScore" } } },
+        { $group: { _id: null, average: { $avg: "$averageScore" } } },
     ]);
     cursor.toArray(function (err, result) {
         if (err) throw err;
-        res.json({"Average Score" : result[0].average * 100/6});
+        res.json({ "Average Score": result[0].average * 100 / 6 });
     });
 });
 
@@ -104,7 +106,7 @@ employeeRoutes.route("/api/employees/dept/all").get(function (req, res) {
     cursor.toArray(function (err, result) {
         if (err) throw err;
         out = {}
-        for(element of result) {
+        for (element of result) {
             out[element._id] = element.average
         }
         res.json(out);
@@ -120,7 +122,7 @@ employeeRoutes.route("/api/employees/dept/:id").get(function (req, res) {
     cursor.toArray(function (err, result) {
         if (err) throw err;
         out = {}
-        for(element of result) {
+        for (element of result) {
             out[element._id] = element.average
         }
         res.json(out);
@@ -136,7 +138,7 @@ employeeRoutes.route("/api/employees/seniority/all").get(function (req, res) {
     cursor.toArray(function (err, result) {
         if (err) throw err;
         out = {}
-        for(element of result) {
+        for (element of result) {
             out[element._id] = element.average
         }
         res.json(out);
@@ -152,7 +154,7 @@ employeeRoutes.route("/api/employees/seniority/:id").get(function (req, res) {
     cursor.toArray(function (err, result) {
         if (err) throw err;
         out = {}
-        for(element of result) {
+        for (element of result) {
             out[element._id] = element.average
         }
         res.json(out);
--- backend/routes/typeform.js
+++ backend/routes/typeform.js
@@ -1,41 +1,42 @@
 const express = require("express");
 const baseForm = require("../form-body")
 const fetch = (...args) =>
-	import('node-fetch').then(({default: fetch}) => fetch(...args));
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
 // apiRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests 
 const typeformRoutes = express.Router();
- 
+
 typeformRoutes.route("/api/typeform/generate").post(async function (req, res) {
-    const {companyname, text, title} = req.body
+  const { companyname, text, title } = req.body
 
-    payload = baseForm
-    payload.welcome_screens[0].title = title
-    payload.welcome_screens[0].properties.description = text
-    payload.title = companyname + " survey"
+  payload = baseForm
+  payload.welcome_screens[0].title = title
+  payload.welcome_screens[0].properties.description = text
+  payload.title = companyname + " survey"
 
-    //Make new survey fitting input data
-    const result = await fetch("https://api.typeform.com/forms", {
-      method : 'POST',
-      headers: {
-        Authorization: "Bearer tfp_7dMvjfhHtcYMFCSMytV8Fsop1UT6mHBVtVW7rdgFfPwN_3w5qgeFDNcwU8J",
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
-    
-    //Get the survey's link
-    const result2 = await fetch("https://api.typeform.com/forms", {
-        method : 'GET',
-        headers: {
-          Authorization: "Bearer tfp_7dMvjfhHtcYMFCSMytV8Fsop1UT6mHBVtVW7rdgFfPwN_3w5qgeFDNcwU8J",
-          'Content-Type': 'application/json'
-        }
-    })
-    body = await result2.json()
-    formId = body.items[0].id
-    res.json("https://3oms1jii0jw.typeform.com/to/" + formId)
+  //Make new survey fitting input data
+  const result = await fetch("https://api.typeform.com/forms", {
+    method: 'POST',
+    headers: {
+      Authorization: "Bearer tfp_7dMvjfhHtcYMFCSMytV8Fsop1UT6mHBVtVW7rdgFfPwN_3w5qgeFDNcwU8J",
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+
+  //Get the survey's link
+  const result2 = await fetch("https://api.typeform.com/forms", {
+    method: 'GET',
+    headers: {
+      Authorization: "Bearer tfp_7dMvjfhHtcYMFCSMytV8Fsop1UT6mHBVtVW7rdgFfPwN_3w5qgeFDNcwU8J",
+      'Content-Type': 'application/json'
+    }
+  })
+  body = await result2.json()
+  formId = body.items[0].id
+  console.log(body.items[0])
+  res.json("https://3oms1jii0jw.typeform.com/to/" + formId)
 
 });
 
