207d09d635b6b65a17d8225a4702214e5baad58f
kishorkumar11 <h.kishorkumar7@gmail.com>
2022-09-13 16:57:01 -0500

    Changed overall font to open sans

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -80,7 +80,7 @@ function Sidebar() {
                       <path className={`fill-current text-slate-600 ${pathname.includes('recommendation') && 'text-teal-500'}`} d="M20 7a.75.75 0 01-.75-.75 1.5 1.5 0 00-1.5-1.5.75.75 0 110-1.5 1.5 1.5 0 001.5-1.5.75.75 0 111.5 0 1.5 1.5 0 001.5 1.5.75.75 0 110 1.5 1.5 1.5 0 00-1.5 1.5A.75.75 0 0120 7zM4 23a.75.75 0 01-.75-.75 1.5 1.5 0 00-1.5-1.5.75.75 0 110-1.5 1.5 1.5 0 001.5-1.5.75.75 0 111.5 0 1.5 1.5 0 001.5 1.5.75.75 0 110 1.5 1.5 1.5 0 00-1.5 1.5A.75.75 0 014 23z" />
                       <path className={`fill-current text-slate-400 ${pathname.includes('recommendation') && 'text-teal-300'}`} d="M17 23a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 010-2 4 4 0 004-4 1 1 0 012 0 4 4 0 004 4 1 1 0 010 2 4 4 0 00-4 4 1 1 0 01-1 1zM7 13a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 110-2 4 4 0 004-4 1 1 0 112 0 4 4 0 004 4 1 1 0 010 2 4 4 0 00-4 4 1 1 0 01-1 1z" />
                   </svg>
-                  <span className="text-sm font-medium ml-3 2xl:opacity-100 duration-200">Recommendation</span>
+                  <span className="text-sm font-medium ml-3 2xl:opacity-100 duration-200">Recommend</span>
                 </div>
               </NavLink>
             </li>
